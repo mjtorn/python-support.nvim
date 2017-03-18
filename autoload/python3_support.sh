440f3c5a4d65cf1e08f3a9905764e2a28fb44280
Markus Törnqvist <mjt@nysv.org>
2017-03-18 16:55:02 +0200

    Work around debian problem

--- autoload/python3_support.sh
+++ autoload/python3_support.sh
@@ -8,8 +8,9 @@ cd $(dirname "${BASH_SOURCE[0]}")
 echo 'initializing python3 virtualenv for neovim'
 
 rm -rf nvim_py3
-python3 -m venv nvim_py3
+python3 -m venv nvim_py3 --without-pip
 . nvim_py3/bin/activate
+curl https://bootstrap.pypa.io/get-pip.py | python
 pip install "$@"
 deactivate
 
